66fc3713302bffe6ab3a45913f41558bef71c64a
AE <austence@sbcglobal.net>
2020-11-08 14:11:19 -0800

    moves Counter function to newly created Counter.js and App.js changes to a display component.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React, { useState } from 'react';
 import './App.css';
+import { Counter } from './Counter'
 
-function Counter() {
-  const [count, setCount] = useState(0);
+function App() {
 
   return (
     <div>
-      <p>You have clicked the button {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click Me
-      </button>
+      <Counter />
     </div>
   );
 }
 
-export default Counter;
+export default App;
---
+++ src/Counter.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You have clicked the button {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click Me
+      </button>
+    </div>
+  );
+}
+
